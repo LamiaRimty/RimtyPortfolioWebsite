15c395671a1eb1e1a1ef90c36e2237fd5e5629c4
Lamia Jabin Rimty <rimtycse@gmail.com>
2023-09-26 11:53:10 +0200

    Added footer section with css styles in Footer compnent

--- rimty-portfolio/src/App.js
+++ rimty-portfolio/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
  import Header from "./components/Header/Header";
 // import Home from "./components/Home/Home";
-// import Footer from "./components/Footer/Footer";
+import Footer from "./components/Footer/Footer";
 //  import Qualification from "./components/Qualification/Qualification";
 //  import Portfolio from "./components/Portfolio/Portfolio";
 
@@ -19,7 +19,7 @@ function App() {
     <Skills />
        <About />
      <Contact />  */}
-      {/* <Footer />  */}
+      <Footer /> 
     </div>
   );
 }
