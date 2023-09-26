249d0c1731cd90d75fdcbb1b3aa4cab8588898da
Lamia Jabin Rimty <rimtycse@gmail.com>
2023-09-26 11:31:07 +0200

    added general css styles to index file

--- rimty-portfolio/src/App.js
+++ rimty-portfolio/src/App.js
@@ -1,10 +1,25 @@
+import "./App.css";
+//  import Header from "./components/Header/Header";
+//  import Home from "./components/Home/Home";
+//  import Footer from "./components/Footer/Footer";
+//  import Qualification from "./components/Qualification/Qualification";
+//  import Portfolio from "./components/Portfolio/Portfolio";
 
-import './App.css';
+// import Skills from "./components/Skills/Skills";
+// import About from "./components/About/About";
+// import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
     <div className="App">
-
+    {/* <Header />
+    <Home />  
+    <Qualification />  
+    <Portfolio />
+    <Skills />
+       <About />
+     <Contact /> 
+      <Footer />  */}
     </div>
   );
 }
